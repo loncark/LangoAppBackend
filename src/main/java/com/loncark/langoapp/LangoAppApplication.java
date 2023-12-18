a11900d5c6208ea8ba1d30ca0688ede5543c7e0b
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2023-12-18 16:46:18 +0100

    Initial commit.

---
+++ src/main/java/com/loncark/langoapp/LangoAppApplication.java
@@ -0,0 +1,13 @@
+package com.loncark.langoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loncark/langoapp/LangoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loncark.langoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
